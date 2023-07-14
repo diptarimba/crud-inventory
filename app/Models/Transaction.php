df89303a90e7e0fdfe0ab2e77d53a100ff219976
Dipta <dipta.harimbawa@jatis.com>
2023-07-14 17:58:52 +0700

    [fix] miss column

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    protected $fillable = ['product_id', 'customer_id', 'supplier_id', ];
+    protected $fillable = ['product_id', 'customer_id', 'supplier_id', 'quantity'];
 }
